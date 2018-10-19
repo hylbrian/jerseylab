0d61d8d77d746bcebb837b802157edd21a86f95c
Brian Hyland <39333309+hylbrian@users.noreply.github.com>
2018-10-19 15:17:47 +0100

    Update app/src/main/java/ie/ul/brianhyland/jerseylab/MainActivity.java

--- app/src/main/java/ie/ul/brianhyland/jerseylab/MainActivity.java
+++ app/src/main/java/ie/ul/brianhyland/jerseylab/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mNameTextView, mNumberTextView;
     private ImageView mJerseyImageView;
     private Integer i;
-    private Item
+    private JerseyItem
     //private Item
     //private Item mCurrentItem;
 
